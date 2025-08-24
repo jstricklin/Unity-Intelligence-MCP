30b8e2fd8fe904f1abd8bca6b48a4b2d7aff75d0
jstricklin <stricklin.jose@gmail.com>
2025-08-24 13:42:55 -0600

    chore: Add using directive for Tools namespace

--- Server~/Core/Services/EditorBridgeClientService.cs
+++ Server~/Core/Services/EditorBridgeClientService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using UnityIntelligenceMCP.Configuration;
+using UnityIntelligenceMCP.Tools;
 
 namespace UnityIntelligenceMCP.Core.Services
 {
